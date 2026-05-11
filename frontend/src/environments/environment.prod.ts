ec593da5154e83424f67948dbf473dffd53e6842
larksenio <jamiller@utpl.edu.ec>
2026-05-11 17:53:13 -0500

    actualizacion

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   apiUrl: 'https://codelab-api-xlub.onrender.com/api',
-  hubUrl: 'https://api.codelab.app/hubs',
+  apiUrl: 'https://codelab-api-xlub.onrender.com/api',
+  hubUrl: 'https://codelab-api-xlub.onrender.com/hubs'
 };
